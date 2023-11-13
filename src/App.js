c14e8a43bf8c90dc092cb16c47f1bc085a121b41
QuiTuan <daylatuan1310@gmail.com>
2023-11-13 09:27:30 +0700

    delete unused files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
+import Register from "./components/Register";
+import Login from "./components/Login";
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
