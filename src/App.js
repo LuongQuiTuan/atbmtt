f839ed5cbce4e86a0d33a5199c67e3271e681beb
QuiTuan <daylatuan1310@gmail.com>
2023-11-27 16:22:32 +0700

    update default router

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={<Dashboard />} />
           <Route path="/login" index element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/dashboard" element={<Dashboard />} />
